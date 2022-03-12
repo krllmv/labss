ae5c4f55e1cbb40833ed1441f26f5a237cdb8981
krllmv <96638714+krllmv@users.noreply.github.com>
2022-03-12 16:48:52 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    scanf("%d", &a);
-    printf(((a<56) && (a>21)) ? "true\n":"false\n");
+    int a[] = {23,5678,2,564,365,77,443};
+    for (int i=0; i<=6; i++) {
+        printf("%d ", a[i]);
+    }
 
-    int b;
-    scanf("%d", &b);
-    b = b & (1 << 111);
-    printf(b != 0 ? "true":"false");
-
+    int n = 2;
+    int C[n][n], A[2][2] = {{1, 1}, {1, 4}}, B[2][2] = {{1, 2}, {4, 1}};
+    for(int i=0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < n; k++)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+    }
+    for (int i=0; i<n; i++) {
+        printf("\n");
+        for (int j=0; j<=1; j++) {
+            printf(" %d", C[i][j]);
+        }
+    }
     return 0;
 }
